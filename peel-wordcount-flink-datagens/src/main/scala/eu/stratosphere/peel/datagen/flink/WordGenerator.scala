559cd72a532aa479fd2d9c4de56d410b41cb96bc
Christoph Bruecke <christoph.bruecke@campus.tu-berlin.de>
2015-07-29 17:29:15 +0200

    Change WordGenerator to sample from dictionary

--- peel-wordcount-flink-datagens/src/main/scala/eu/stratosphere/peel/datagen/flink/WordGenerator.scala
+++ peel-wordcount-flink-datagens/src/main/scala/eu/stratosphere/peel/datagen/flink/WordGenerator.scala
@@ -2,50 +2,58 @@ package eu.stratosphere.peel.datagen.flink
 
 import eu.stratosphere.peel.datagen.util.RanHash
 import org.apache.flink.api.scala._
+import org.apache.flink.core.fs.FileSystem
 import org.apache.flink.util.NumberSequenceIterator
-
-import scala.io.{Codec, Source}
 
 object WordGenerator {
 
+  type Distribution = (RanHash, Int) => Int
+
   val SEED = 1010
 
-  def sampleWord (wordlist : Array[String]) (index : (RanHash, Int) => Int) (seed : Long) (seqNo : Long) : String = {
-    val ran = new RanHash(seed)
-    ran.skipTo(seqNo)
-    wordlist(index(ran, wordlist.length))
-  }
+  def main(args: Array[String]): Unit = {
 
-  def main(args : Array[String]) : Unit = {
-
-    if (args.length != 4) {
-      return
+    if (args.length != 5) {
+      Console.err.println("Usage: <jar> sizeOfDictionary numberOfWorkers coresPerWorker tuplesPerTask outputPath")
+      System.exit(-1)
     }
-    val numberOfWorkers = args(0).toInt
-    val coresPerWorker = args(1).toInt
-    val tuplesPerTask = args(2).toInt
-    val outputPath = args(3)
 
-    val dop = coresPerWorker * numberOfWorkers
-    val n = dop * tuplesPerTask
+    val sizeOfDictionary = args(0).toInt
+    val numberOfWorkers  = args(1).toInt
+    val coresPerWorker   = args(2).toInt
+    val tuplesPerTask    = args(3).toInt
+    val outputPath       = args(4)
 
-    // index function
-    val distribution : (RanHash, Int) => Int = { (ran, len) => Math.floor(ran.next() * len + 0.5).toInt }
+    // TODO add distribution and dictionarySize parameters
 
-    // load the word list from the resources folder and partially apply the sampleWord function to it
-    val wordlist = Source.fromFile(getClass.getResource("/wordlist").getPath, Codec.UTF8.toString()).getLines().toArray
-    val word = sampleWord (wordlist) (distribution) (SEED) _
+    val dop              = coresPerWorker * numberOfWorkers
+    val N                = dop * tuplesPerTask
 
-    val env = ExecutionEnvironment.getExecutionEnvironment
-    val randomWords = env
+    // generate dictionary of random words
+    implicit val dictionary = new Dictionary(SEED, sizeOfDictionary).words
+
+    implicit val distribution = uniform _ // TODO pick from 'uniform _', 'pareto _' or 'gaussian _'
+
+    val environment = ExecutionEnvironment.getExecutionEnvironment
+
+    environment
       // create a sequence [1 .. N] to create N words
-      .fromParallelCollection(new NumberSequenceIterator(1, n))
+      .fromParallelCollection(new NumberSequenceIterator(1, N))
       // set up workers
       .setParallelism(dop)
       // map every n <- [1 .. N] to a random word sampled from a word list
       .map(i => word(i))
+      // write result to file
+      .writeAsText(outputPath, FileSystem.WriteMode.OVERWRITE)
+    environment.execute(s"WordGenerator[$N]")
+  }
 
-    randomWords.writeAsText(outputPath)
-    env.execute()
+  def word(i: Long)(implicit dictionary: Array[String], distribution: Distribution) = {
+    dictionary(distribution(new RanHash(SEED + i), dictionary.length))
+  }
+
+  // index function
+  def uniform(ran: RanHash, len: Int): Int = {
+    Math.floor(ran.next() * (len - 1) + 0.5).toInt
   }
 }
